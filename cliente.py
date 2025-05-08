aac9de01621639d4edb48e5c2e2c41f7e53749e5
Andrey <andreyfernandezg@gmail.com>
2025-05-08 10:11:51 +0200

    Client working

--- cliente.py
+++ cliente.py
@@ -1,17 +1,17 @@
 import requests
 import json
-from program import mostrar
 from app import LOCAl_IP
 from app import DEFAULT_PORT
-import socket
 
 
 def main():
-    URL = f'http://{LOCAl_IP}:{DEFAULT_PORT}/'
+    URL = f"http://{LOCAl_IP}:{DEFAULT_PORT}/"
     response = requests.get(URL)
     tablero = response.json()
+    # Print every call in console
     print(tablero)
+    return tablero
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
